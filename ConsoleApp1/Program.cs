30712c8f48af8c185c09db35c16dfbe082b15989
shahnaz242002 <shahnazavazova@gmail.com>
2023-04-04 18:46:53 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+while (number1 != 0)
+{
+    int ostatok1 = number1 % 10;
+    int tmp = number2;
+
+    while (tmp != 0)
+    {
+        int ostatok2 = tmp % 10;
+
+
+        if (ostatok1 == ostatok2)
+        {
+            Console.WriteLine("Да");
+            return;
+        }
+
+        tmp /= 10;
+
+    }
+
+    number1 /= 10;
+}
+
+Console.WriteLine("Нет");
---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+Console.OutputEncoding = Encoding.UTF8; 
+
+Console.WriteLine("Введите два числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+for (int i = number1; i <= number2; i++)
+{
+    int a = 2;
+    while (i>=a)
+    {
+        number1 % a == 0
+    }
+}
