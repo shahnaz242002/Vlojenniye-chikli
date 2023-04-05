fb3ae81692861d2b3ed4ab27ea9fb2c4fa93ae58
shahnaz242002 <shahnazavazova@gmail.com>
2023-04-05 18:52:13 +0400

    добавила второе задание

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -6,9 +6,21 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 for (int i = number1; i <= number2; i++)
 {
-    int a = 2;
-    while (i>=a)
+    if (i == 2 || i == 3 ||i==5 || i==7)
     {
-        number1 % a == 0
+        Console.WriteLine($"{i}-простое число");
     }
+    
+     else if (i %2==0|| i%3==0 ||i%5==0 || i%7==0)
+    {
+        Console.WriteLine($"{i}-сложное число");
+       
+    }
+    else
+    {
+        Console.WriteLine($"{i}-простое число");
+
+    }
+
+
 }
